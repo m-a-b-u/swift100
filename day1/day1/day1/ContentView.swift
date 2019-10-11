46eb3a751757db23021ec1cb5df86a44d83934ec
Manuel Burgschachner <manuel@burgschachner.at>
2019-10-11 20:09:34 +0200

    Hello World, first commit

---
+++ day1/day1/day1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  day1
+//
+//  Created by Manuel Burgschachner on 11.10.19.
+//  Copyright © 2019 Manuel Burgschachner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day1/day1/day1Tests/day1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  day1Tests.swift
+//  day1Tests
+//
+//  Created by Manuel Burgschachner on 11.10.19.
+//  Copyright © 2019 Manuel Burgschachner. All rights reserved.
+//
+
+import XCTest
+@testable import day1
+
+class day1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
