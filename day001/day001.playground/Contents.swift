eb5ecf9710093b986236929eed93b605d9442e06
Manuel Burgschachner <manuel.burgschachner@runtastic.com>
2020-06-23 19:50:13 +0200

    Swift in 100 days, day001 with variables, simple data types and string interpolation

---
+++ day001/day001.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+var str = "Hello, playground"
+
+str = "Goodbye"
+
+var age = 38
+
+var population = 8_000_000
+
+var str1 = """
+this goes
+over mutliple
+lines
+"""
+
+var str2 = """
+this goes \
+over multiple \
+lines
+"""
+
+var pi = 3.141
+var awesome = true
+
+
+var score = 85
+var str3 = "Your score was \(score)"
+
+let taylor = "swift"
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let tayloerRocks: Bool = true
+
