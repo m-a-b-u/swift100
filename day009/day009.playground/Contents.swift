cc977d3a5714b5441680057bd24429ed4bef18b5
Manuel Burgschachner <manuel.burgschachner@runtastic.com>
2020-06-30 21:42:55 +0200

    day009 also done... lol did not know that... but still the test and optionals for day 7 8 9 open... so will do this tomorrow

---
+++ day009/day009.playground/Contents.swift
@@ -0,0 +1,66 @@
+import UIKit
+
+struct User {
+    var username: String
+
+    init() {
+        username = "Anonymous"
+        print("Creating a new user!")
+    }
+}
+
+var user = User()
+user.username = "twostraws"
+
+struct Person {
+    var name: String
+
+    init(name: String) {
+        print("\(name) was born!")
+        self.name = name
+    }
+}
+
+struct FamilyTree {
+    init() {
+        print("Creating family tree!")
+    }
+}
+
+struct Person2 {
+    var name: String
+    lazy var familyTree = FamilyTree()
+
+    init(name: String) {
+        self.name = name
+    }
+}
+
+var ed = Person2(name: "Ed")
+ed.familyTree
+
+struct Student {
+    static var classSize = 0
+    var name: String
+
+    init(name: String) {
+        self.name = name
+        Student.classSize += 1
+    }
+}
+
+let edd = Student(name: "Edd")
+let taylor = Student(name: "Taylor")
+print(Student.classSize)
+
+struct Person3 {
+    private var id: String
+
+    init(id: String) {
+        self.id = id
+    }
+
+    func identify() -> String {
+        return "My social security number is \(id)"
+    }
+}
