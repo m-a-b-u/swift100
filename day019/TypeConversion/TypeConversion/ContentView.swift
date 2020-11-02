7f5fdf041ed8d1455e5da793e7f7fbb976b16629
Manuel Burgschachner <manuel.burgschachner@runtastic.com>
2020-11-02 19:20:29 +0100

    day019 added

---
+++ day019/TypeConversion/TypeConversion/ContentView.swift
@@ -0,0 +1,98 @@
+//
+//  ContentView.swift
+//  TypeConversion
+//
+//  Created by Manuel Burgschachner on 02.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var distanceText = ""
+    @State private var typeFrom = 2
+    @State private var typesFrom = ["mm","cm","dm","m","km"]
+
+    @State private var typeTo = 2
+    @State private var typesTo = ["mm","cm","dm","m","km"]
+    
+    
+    var fromDistance: Double {
+        // calculate the input distance to mm
+        
+        let distance = Double(distanceText) ?? 0
+        
+        switch typeFrom {
+        case 0:
+            return distance
+            
+        case 1:
+            return distance*10
+        case 2:
+            return distance*100
+        case 3:
+            return distance*1000
+        case 4:
+            return distance*10000
+        default:
+            return 0
+        }
+    }
+    
+    var divider: Double {
+        switch typeTo {
+        case 0:
+            return 1
+            
+        case 1:
+            return 10
+        case 2:
+            return 100
+        case 3:
+            return 1000
+        case 4:
+            return 1000000
+        default:
+            return 0
+        }
+        
+    }
+    
+    var body: some View {
+        
+        NavigationView{
+        
+            Form{
+                Section(header: Text("Input")){
+                    TextField("Enter distance", text: $distanceText)
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section(header: Text("From")){
+                    Picker("Select source type", selection: $typeFrom){
+                        ForEach(0 ..< typesFrom.count){
+                            Text("\(self.typesFrom[$0])")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("To")){
+                    Picker("Select destination type", selection: $typeTo){
+                        ForEach(0 ..< typesTo.count){
+                            Text("\(self.typesTo[$0])")
+                        }
+
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Output")){
+                    Text("\(fromDistance/divider,specifier: "%.2f")")
+                }
+            }.navigationBarTitle("Type Conversion")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day019/TypeConversion/TypeConversion/TypeConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypeConversionApp.swift
+//  TypeConversion
+//
+//  Created by Manuel Burgschachner on 02.11.20.
+//
+
+import SwiftUI
+
+@main
+struct TypeConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
